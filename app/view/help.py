c019fdb2dedbe69f2b3ed6ce6dd806b115d26514
xjiaf <mofajaph@gmail.com>
2026-03-04 19:01:50 +0000

    udpate descript

--- app/view/help.py
+++ app/view/help.py
@@ -13,11 +13,10 @@ class Help(QFrame):
         self._is_non_chinese_ui = is_non_chinese_ui_language()
         self.setObjectName(text.replace(' ', '-'))
 
-        # 1. 搭建骨架 (原 View 的职责)
         self._setup_ui()
-        # 2. 填充内容 (原 Controller 的职责)
+
         self._load_markdown()
-        # 3. 穿上衣服
+
         StyleSheet.HELP_INTERFACE.apply(self)
 
     def _setup_ui(self):
@@ -36,10 +35,6 @@ class Help(QFrame):
     def _load_markdown(self):
         """负责加载业务数据"""
         markdown_path = './docs/help_en.md' if self._is_non_chinese_ui else './docs/help.md'
-
-        # 兜底逻辑：如果英文文档不存在，回退到中文文档
-        if not os.path.exists(markdown_path):
-            markdown_path = './docs/help.md'
 
         try:
             with open(markdown_path, 'r', encoding='utf-8') as file:
