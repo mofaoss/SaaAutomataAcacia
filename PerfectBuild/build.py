1c71a1c62c1dcb9f9cbb710d5f4b80b90adbd618
mofaoss <mofajaph@gmail.com>
2026-03-14 13:05:09 +0000

    update

--- PerfectBuild/build.py
+++ PerfectBuild/build.py
@@ -206,9 +206,11 @@ class PerfectBuild:
             "--include-package=app.features.modules",
 
             # 精简 Qt 插件
-            "--noinclude-qt-plugins=webengine,multimedia,sql,test,sensorkit,position,location,bluetooth,nfc,serialport,printsupport,dbus,pdf",
+            "--noinclude-qt-plugins=qml,webengine,network,multimedia,sql,test,sensorkit,position,location,bluetooth,nfc,serialport,websockets,printsupport,dbus,xml,pdf",
             # 无用且极其臃肿的 Qt 底层 DLL
+            "--noinclude-dlls=*qt6network*.dll",
             "--noinclude-dlls=*qt6pdf*.dll",
+            "--noinclude-dlls=*qt6webengine*.dll",
 
             # 精简 OpenCV / onnxruntime 相关 DLL
             "--noinclude-dlls=cv2/opencv_videoio_ffmpeg*.dll",
